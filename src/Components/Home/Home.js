1b7aed4f2b30486d127187501b57b9cbd50a85b0
Kamran <kgali21@gmail.com>
2020-05-19 11:59:10 -0700

    styling form box

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,13 +2,18 @@ import React from 'react';
 import SymbolRequestForm from '../SymbolRequestForm/SymbolRequestForm';
 import Symbols from '../SymbolDisplay/Symbols';
 import SymbolRemoveForm from '../SymbolRequestForm/SymbolRemoveForm';
+import styles from './Home.module.css';
 
 const Home = () => (
-    <>
+    <div className={styles.App}>
+    <div className={styles.Form}>
+        <p>Add a stock symbol to start tracking</p>
         <SymbolRequestForm />
+        <p>Remove a stock symbol</p>
         <SymbolRemoveForm />
+    </div>
         <Symbols />
-    </>
+    </div>
 )
 
 export default Home;
