0da47d2885aad5601d583160215dee7ba24eacdd
Kamran <kgali21@gmail.com>
2020-05-19 15:36:41 -0700

    responsive design set in place

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './Header.module.css';
 
 const Header = () => (
-    <>
+    <span className={styles.ImgContainer}>
         <img className={styles.Logo} src='https://www.orbisfn.com/img/orbislogo.svg' alt='Orbis Logo' />
-    </>
+    </span>
 );
 
 export default Header;
