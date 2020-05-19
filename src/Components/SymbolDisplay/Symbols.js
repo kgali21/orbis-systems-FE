6eff6214d43b9d62f28a629484e547e071241c00
Kamran <kgali21@gmail.com>
2020-05-19 06:40:43 -0700

    now updates state of the object every 10 seconds; core functionality done

--- src/Components/SymbolDisplay/Symbols.js
+++ src/Components/SymbolDisplay/Symbols.js
@@ -1,25 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import SymbolCard from './SymbolCard';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { getSymbolObject } from '../../selectors/symbolsSelectors';
+import { updateSymbol } from '../../actions/symbolActions';
+import useInterval from '@use-it/interval';
 
 const Symbols = () => {
-    // const [symbols, setSymbols] = useState([]);
+    const dispatch = useDispatch();
     const symbolsItems = useSelector(getSymbolObject);
 
-    // useEffect((symbolsItems) => {
-    //     setInterval(() =>{
-    //         symbolsItems(setSymbols)
-    //     }, 5000)
-    //     console.log(symbols, 'symbols')
-    // }, [symbolsItems])
-
+    useInterval(() => {
+        dispatch(updateSymbol(symbolsItems))
+    }, 10000)
 
     const dataElements = symbolsItems.map((data, i) => (
         <li key={i}>
             <SymbolCard  symbol={data.symbol.symbol} tweet={data.messages[0].body} tweetCount={data.messages.length}/>
         </li>
-    ))
+    )).slice(0, 5);
 
     return (
     <ul>
